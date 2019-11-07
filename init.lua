7b476fa121a95a33fec3fd9b8db35ea92280d521
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2019-11-07 22:45:06 +0100

    polish site and init

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-
-
 --[[
 
 
@@ -20,18 +18,19 @@ mytimer:start()
 function configureAp()
     enduser_setup.manual(true)
     enduser_setup.start(
-      function()
-        print("Connected to WiFi as:" .. wifi.sta.getip());
-        if wifi.sta.config(wifi.sta.getconfig(true)) then-- store config in flash
-            print("Succesfully configured sta (to flash)");
-        else
-            print("Error configuring sta");
-        end
-      
-      end,
-      function(err, str)
-        print("enduser_setup: Err #" .. err .. ": " .. str);
-      end
+            function()
+                print("Connected to WiFi as:" .. wifi.sta.getip());
+                if wifi.sta.config(wifi.sta.getconfig(true)) then
+                    -- store config in flash
+                    print("Succesfully configured sta (to flash)");
+                else
+                    print("Error configuring sta");
+                end
+
+            end,
+            function(err, str)
+                print("enduser_setup: Err #" .. err .. ": " .. str);
+            end
     )
 end
 
@@ -39,13 +38,15 @@ wifi.sta.autoconnect(0) -- disable auto connect beacause for some reason it does
 
 
 name, password, channel = wifi.sta.getconfig()
-if channel == nil then
-  channel = 6
+print("starting, name: " .. name .. ", password: " .. password .. ", channel: " .. channel)
+if channel == nil or channel == 0 then
+    channel = 6
 end
+print("channel: " .. channel)
 
 wifi.setmode(wifi.STATIONAP, false) -- don't store it in flash
 
-if wifi.ap.config({ssid="MyPersonalSSID", pwd="haslo123456", auth=wifi.WPA2_PSK, save=false, channel=channel}) then
+if wifi.ap.config({ ssid = "MyPersonalSSID", pwd = "haslo123456", auth = wifi.WPA2_PSK, save = false, channel = channel }) then
     print("Succesfully configured ap");
 else
     print("Error configuring ap");
@@ -54,17 +55,18 @@ end
 configureAp()
 
 function tryToConnect()
-  ip, nm, gw = wifi.sta.getip()
-  if ip == nil then
-      print("tryingToConnect");
-    wifi.sta.connect()
-  else
-      print("connected already, ip:" .. ip .. " gw:" .. gw .. " nm:" .. nm);
-  end
+    name, password, channel = wifi.sta.getconfig()
+    print("periodic wifi check, name: " .. name .. ", password: " .. password .. ", channel: " .. channel)
+    ip, nm, gw = wifi.sta.getip()
+    if ip == nil then
+        print("tryingToConnect");
+        wifi.sta.connect()
+    else
+        print("connected already, ip:" .. ip .. " gw:" .. gw .. " nm:" .. nm);
+    end
 end
 
 local mytimer = tmr.create()
-
 
 mytimer:register(10000, tmr.ALARM_AUTO, tryToConnect)
 mytimer:start()
